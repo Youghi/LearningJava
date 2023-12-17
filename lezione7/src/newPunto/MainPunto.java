7b7e392cc084cf7aa931048d7d719b92c68f1957
Youghi <zykov.igor.95@gmail.com>
2023-12-17 22:38:38 +0100

    added punto exercise

---
+++ lezione7/src/newPunto/MainPunto.java
@@ -0,0 +1,50 @@
+package newPunto;
+
+/*
+	1. Definisci una classe Punto che abbia come attributi le coordinate x e y di un punto.
+	
+	Nello sviluppo dei punti successivi considera solo coordinate positive.
+
+	9. Nella classe Punto definisco una funzione che stampa le coordinate di un punto
+	
+	10. Nella classe Punto definisco il metodo PuntiUguali() che confronta le coordinate di due
+		punti e dirà se i punti sono uguali oppure no.
+	
+	11. Nella classe Punto definisco un metodo che calcola la distanza di un punto da un altro
+	
+	12. Nel metodo main genero due punti generando casualmente le coordinate del punto che
+	possono avere valori interi compresi fra 0 e 10 E utilizzando la funzione appena creata ne faccio il confronto stabilendo se sono diversi
+	o uguali.
+	
+	13. Nel classe Punto definisco una funzione generaPunto() che genera e ritorna un oggetto
+		di classe Punto generando casualmente le coordinate del punto che possono avere
+		valori interi compresi fra 0 e 10 (definisco questo metodo static poiché generando un
+		oggetto di classe Punto non dovrà essere associato a nessun Punto specifico, ma è
+		bene che rimanga un’azione specifica della classe Punto)
+	
+	14. Nel metodo Main invoco la funzione genera Punto per creare un oggetto di classe Punto
+		e ne stampo le coordinate usando la funzione opportuna
+	
+	15. Nello stesso package, creo una classe Triangolo i cui attributi sono tre variabili di classe
+		Punto, una per ogni vertice del triangolo. Si passino al costruttore della classe i tre punti
+		vertici del punto)
+	
+	16. Nella classe Triangolo definisco la funzione che calcola il perimetro del triangolo
+	
+	17. Nella classe Triangolo definisco una funzione che verifica se il triangolo è equilatero,
+		isoscele o scaleno.
+	
+	18. Nella classe Triangolo definisco la funzione che calcola l’area se il triangolo è equilatero
+	
+	19. Nel metodo main genero i tre punti vertici (utilizzando la funzione opportuna) di un
+		triangolo e poi il triangolo stesso, ne calcolo il perimetro e l’area se il triangolo è
+		equilatero
+ */
+public class MainPunto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ lezione7/src/newPunto/Triangolo.java
@@ -0,0 +1,5 @@
+package newPunto;
+
+public class Triangolo {
+
+}
